ec1a71e6694a2ce2be742f87338afc9be89e6003
Janne <janne.viljanen@hotmail.com>
2026-04-08 12:51:10 +0300

    Base for AB#59

---
+++ reroute.java
@@ -0,0 +1,15 @@
+
+public class Reroute {
+
+    private route route;
+
+    public road(final Route) {
+        this.route = var1;
+        if (var1 >= 0) {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println("Hello");
+            }
+        }
+
+    }
+}
