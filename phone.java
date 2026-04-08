eef7899a1eb67f4ee59886595bfdd7c0a49048d6
ultsuIT <ullamaija.alander@outlook.com>
2026-04-08 19:26:16 +0300

    phone.java created
    Working on AB#65 #17

---
+++ phone.java
@@ -0,0 +1,10 @@
+public class phone {
+    public static void main(String[] args) throws Exception {
+        
+        int phone = 100007;
+            
+        if (phone == 100007) {
+            System.out.println("Hello 007.");
+        }
+    }
+}
